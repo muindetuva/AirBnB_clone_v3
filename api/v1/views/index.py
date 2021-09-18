e79391c20fac8ed7c78358acc7eddafa3a8667c8
keira-claudette <claudette.keira@gmail.com>
2021-09-18 09:00:15 +0000

    initial commit

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -2,8 +2,20 @@
 """ index.py"""
 from api.v1.views import app_views
 from flask import jsonify
+from models import storage
 
 
 @app_views.route("/status")
 def status_ok():
     return jsonify({"status": "OK"})
+
+
+@app_views.route("/stats")
+def obj_stats():
+    objs = {"amenities": storage.count("Amenities"),
+            "cities": storage.count("Cities"),
+            "reviews": storage.count("Reviews"),
+            "states": storage.count("States"),
+            "users": storage.count("Users")}
+    print("This is objs {}/n".format(objs))
+    return jsonify(objs)
