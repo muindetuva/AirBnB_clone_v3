4d26ee3a3a7caf0b6bbe6176bad8f7697043cbac
Alfred Tuva <muindetuva@gmail.com>
2021-09-18 15:11:20 +0300

    Add a get method for one state

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -1,13 +1,26 @@
 #!/usr/bin/python3
 """ States view"""
 from api.v1.views import app_views
-from flask import jsonify
+from flask import jsonify, abort
 from models import storage
 
 
-@app_views.route("/states", strict_slashes=False)
+@app_views.route("/states")
 def all_states():
     states_list = []
     for state in storage.all("State").values():
         states_list.append(state.to_dict())
     return jsonify(states_list)
+
+
+@app_views.route("/states/<state_id>")
+def state(state_id):
+    state = storage.get("State", state_id)
+    if not state:
+        abort(404)
+    return jsonify(state.to_dict())
+
+
+#@app_views.route("/states/<state_id>", methods=['DELETE'])
+#def delete_state(state_id):
+
