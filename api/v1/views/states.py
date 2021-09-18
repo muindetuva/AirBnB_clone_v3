993d95f1c094157078bc8e11946952dcdbff9f9a
keira-claudette <claudette.keira@gmail.com>
2021-09-18 10:07:45 +0000

    initial commit

---
+++ api/v1/views/states.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+""" States view"""
+from api.v1.views import app_views
+from flask import jsonify
+from models import storage
+
+
+@app_views.route("/states")
+def all_states():
+    states_list = []
+    for state in storage.get("State").values():
+        states_list.append(state.to_dict())
+    return jsonify(states_list)
